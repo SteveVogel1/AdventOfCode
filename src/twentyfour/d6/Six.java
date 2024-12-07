da603de7547ddc44af955b3bd5af6367b6c02418
SteveVogel1 <steve.vogel@students.fhnw.ch>
2024-12-07 15:05:50 +0100

    2024d6 task b

--- src/twentyfour/d6/Six.java
+++ src/twentyfour/d6/Six.java
@@ -15,13 +15,13 @@ public class Six implements DailyTask {
         long result = 0;
         int xCurrent = 0;
         int yCurrent = 0;
-        char[][] grid = new char[input.size()][input.getFirst().length()];
+        String[][] grid = new String[input.size()][input.getFirst().length()];
         for(int i = 0; i < input.size(); i++){
-            grid[i] = input.get(i).toCharArray();
+            grid[i] = input.get(i).split("");
             if(input.get(i).contains("^")){
                 yCurrent = i;
                 xCurrent = input.get(i).indexOf('^');
-                grid[yCurrent][xCurrent] = '.';
+                grid[yCurrent][xCurrent] = ".";
             }
         }
 
@@ -32,8 +32,8 @@ public class Six implements DailyTask {
 
         outerLoop:
         while(true) {
-            if(grid[yCurrent][xCurrent] == '.') {
-                grid[yCurrent][xCurrent] = 'X';
+            if(grid[yCurrent][xCurrent].equals(".")) {
+                grid[yCurrent][xCurrent] = "X";
                 result++;
             }
 
@@ -61,38 +61,107 @@ public class Six implements DailyTask {
         if(DEBUGGING) {
             printGrid(grid);
         }
-
-// up - width
-// down + width
-//left - width
-// right + width
-
         return String.valueOf(result);
-    }
-
-    private boolean isBlocked(char[][] grid, int x, int y){
-        return grid[y][x] == '#';
-    }
-    private boolean inBound(char[][] grid, int x, int y){
-        return !(x < 0 | y < 0) && x < grid[0].length && y < grid.length;
     }
 
     public String taskB(List<String> input){
         long result = 0;
-
+        for(int y = 0; y < input.size(); y++) {
+            for(int x = 0; x <input.getFirst().length(); x++){
+                if (checkForLoop(input, x, y)) {
+                    if(DEBUGGING) {
+                        System.out.println("Result Nr. " + result);
+                        System.out.println("X: " + x + " Y: " + y);
+                    }
+                    result++;
+                }
+            }
+        }
         return String.valueOf(result);
     }
 
-    private void printGrid(char[][] grid){
-        for (char[] chars : grid) {
+    private boolean isBlocked(String[][] grid, int x, int y){
+        return hasChar(grid, "#", x, y);
+    }
+
+    private boolean hasChar(String[][] grid, String c, int x, int y){
+        return grid[y][x].equals(c);
+    }
+
+    private boolean inBound(String[][] grid, int x, int y){
+        return !(x < 0 | y < 0) && x < grid[0].length && y < grid.length;
+    }
+
+    private void printGrid(String[][] grid){
+        System.out.println("-------------------------------------");
+        System.out.println("");
+        for (String[] chars : grid) {
             for (int j = 0; j < grid[0].length; j++) {
                 System.out.print(chars[j]);
             }
             System.out.println();
         }
-
+        System.out.println("");
     }
 
+    private boolean checkForLoop(List<String> input, int x, int y){
+        int xCurrent = 0;
+        int yCurrent = 0;
+        String[][] grid = new String[input.size()][input.getFirst().length()];
+        for(int i = 0; i < input.size(); i++){
+            grid[i] = input.get(i).split("");
+            if(input.get(i).contains("^")){
+                yCurrent = i;
+                xCurrent = input.get(i).indexOf('^');
+                grid[yCurrent][xCurrent] = ".";
+            }
+        }
+
+        grid[y][x] = "#";
+
+        int xNext;
+        int yNext;
+
+        int xLastTurn = -1;
+        int yLastTurn = -1;
+
+        Direction currentDirection = Direction.UP;
+
+        int change = 0;
+        outerLoop:
+        while(true) {
+            do {
+                yNext = yCurrent;
+                xNext = xCurrent;
+                switch (currentDirection) {
+                    case UP -> yNext = yCurrent - 1;
+                    case DOWN -> yNext = yCurrent + 1;
+                    case RIGHT -> xNext = xCurrent + 1;
+                    case LEFT -> xNext = xCurrent - 1;
+                }
+                if(!inBound(grid, xNext, yNext)){
+                    break outerLoop;
+                }
+                if (isBlocked(grid, xNext, yNext)) {
+                    currentDirection = Direction.getNextDirection(currentDirection);
+                    if(!hasChar(grid, ".", xCurrent, yCurrent) && !(xLastTurn == xCurrent && yLastTurn == yCurrent)){
+                        if(DEBUGGING) {
+                            printGrid(grid);
+                        }
+                        return true;
+                    }
+
+                    xLastTurn = xCurrent;
+                    yLastTurn = yCurrent;
+                    grid[yCurrent][xCurrent] = String.valueOf(change);
+                    change++;
+                }
+            } while(isBlocked(grid, xNext, yNext));
+            yCurrent = yNext;
+            xCurrent = xNext;
+        }
+        return false;
+    }
 
     enum Direction {
         UP, RIGHT, DOWN, LEFT;
