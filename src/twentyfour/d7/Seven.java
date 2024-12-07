31d0731a9c4bfdb6ca83e025a0fb1b84139ce8cc
SteveVogel1 <steve.vogel@students.fhnw.ch>
2024-12-07 17:57:36 +0100

    2024d7 task a & part of b

--- src/twentyfour/d7/Seven.java
+++ src/twentyfour/d7/Seven.java
@@ -14,16 +14,37 @@ public class Seven implements DailyTask {
         long result = 0;
 
         for(String line : input) {
-            String[] x = line.split(" ");
-
+            String[] values = line.split(" ");
+            long expected = Long.parseLong(values[0].substring(0, values[0].length()-1));
+            if(check(expected, 0, false, List.of(values).subList(1, values.length).stream().map(Integer::valueOf).toList())){
+                result += expected;
+            }
         }
 
         return String.valueOf(result);
     }
 
+    private boolean check(long expected, long current, boolean concat, List<Integer> values){
+        if(expected == current){
+            return true;
+        }
+        if(values.isEmpty() || current > expected){
+            return false;
+        }
+
+        return check(expected, current + values.getFirst(), concat, values.subList(1,values.size())) || check(expected, current * values.getFirst(), concat, values.subList(1,values.size()))
+                || (concat);
+    }
+
     public String taskB(List<String> input){
         long result = 0;
-
+        for(String line : input) {
+            String[] values = line.split(" ");
+            long expected = Long.parseLong(values[0].substring(0, values[0].length()-1));
+            if(check(expected, 0, true, List.of(values).subList(1, values.length).stream().map(Integer::valueOf).toList())){
+                result += expected;
+            }
+        }
         return String.valueOf(result);
     }
 }
