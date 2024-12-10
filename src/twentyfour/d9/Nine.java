6f365cd9a4b88d6f2fe46a93016bcdddebd968f1
SteveVogel1 <steve.vogel@students.fhnw.ch>
2024-12-10 23:08:45 +0100

    2024d9 task b

--- src/twentyfour/d9/Nine.java
+++ src/twentyfour/d9/Nine.java
@@ -24,6 +24,12 @@ public class Nine implements DailyTask {
 
     public String taskB(List<String> input){
         long result = 0;
+        String line = input.getFirst();
+
+        ArrayList<String> system = getSystem(line);
+        defragmentationBlock(system);
+
+        result = getResult(system, result);
         return String.valueOf(result);
     }
 
@@ -47,6 +53,45 @@ public class Nine implements DailyTask {
         }
     }
 
+    private void defragmentationBlock(ArrayList<String> system) {
+        int leftIndex = 0;
+        int rightIndex = system.size() -1;
+        while(rightIndex > 0){
+            int lengthBlock = 0;
+            while(system.get(rightIndex).equals(".")){
+                rightIndex--;
+            }
+            while(rightIndex-lengthBlock > 0 && system.get(rightIndex).equals(system.get(rightIndex-lengthBlock))){
+                lengthBlock++;
+            }
+
+            while(true){
+                int lengthSpace = 1;
+                while(!system.get(leftIndex).equals(".")) {
+                    leftIndex++;
+                }
+
+                while(leftIndex+lengthSpace < rightIndex && system.get(leftIndex+lengthSpace).equals(".")){
+                    lengthSpace++;
+                }
+                if(leftIndex+lengthSpace > rightIndex){
+                    break;
+                }
+                if(lengthSpace >= lengthBlock){
+                    for(int s = 0; s < lengthBlock; s++) {
+                        system.set(leftIndex + s, system.get(rightIndex -s));
+                        system.set(rightIndex -s, ".");
+                    }
+                    break;
+                }
+                leftIndex = leftIndex + lengthSpace;
+            }
+
+            leftIndex = 0;
+            rightIndex -= lengthBlock;
+        }
+    }
+
     private ArrayList<String> getSystem(String line) {
         boolean isFile = true;
         long index = 0;
@@ -65,7 +110,6 @@ public class Nine implements DailyTask {
         }
         return system;
     }
-
 
     private long getResult(ArrayList<String> system, long result) {
         for(int i = 0; i < system.size(); i++){
